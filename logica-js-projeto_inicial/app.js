0ebe8bfaf29af99acbc3511a30f474f55200f958
Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>
2024-02-23 19:36:39 -0300

    123

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,6 @@
+alert("Boas vindas ao jogo do número secreto!");
+let numeroScreto = 29;
+let chute = prompt("Escolha um numero de 1 a 30");
+if (numeroScreto == chute) {
+  console.log("Voce descobriu o numero secreto (29)");
+}
